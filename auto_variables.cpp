42eb715ad26801793df18dac763d74a00d9c0b33
imFAHADcom <contact@imfahad.com>
2026-07-30 19:10:13 +0300

    UPD auto_variables.cpp

--- auto_variables.cpp
+++ auto_variables.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main(){
 
-	auto x = 20;
-	auto y = 50.25;
-	auto z = "i'm Fahad";
+	auto x = 20; // x is an integar variable
+	auto y = 50.25; // y is a float variable
+	auto z = "i'm Fahad"; // z is a string variable
+	auto a = 'F'; // a is a char variabale
+	auto b = true; // b is a bool variable
 
 	cout << x << "\n";
 	cout << y << "\n";
-	cout << z << endl;
+	cout << z << "\n";
+	cout << a << "\n";
+	cout << b << endl;
 
 	return 0;
 }
