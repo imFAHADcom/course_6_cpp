af1eb64a336644e5ac31d290c9d358db663686fa
imFAHADcom <contact@imfahad.com>
2026-07-30 18:52:35 +0300

    ADD auto_variables.cpp

---
+++ auto_variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	auto x = 20;
+	auto y = 50.25;
+	auto z = "i'm Fahad";
+
+	cout << x << "\n";
+	cout << y << "\n";
+	cout << z << endl;
+
+	return 0;
+}
