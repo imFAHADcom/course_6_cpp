81953a9b8eabae066a9123a4c91e99bc04ac3cc5
imFAHADcom <contact@imfahad.com>
2026-09-21 16:48:26 +0300

    ADD vector_iterators.cpp

---
+++ vector_iterators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	// declare vector
+	vector<int> num{1, 2, 3, 4, 5};
+
+	// declare iterator
+	vector<int>::iterator iter;
+
+	// use iterator with for loop
+	for(iter = num.begin(); iter != num.end(); ++iter){
+		cout << *iter << " ";
+
+	}
+	
+	cout << endl;
+
+	return 0;
+}
